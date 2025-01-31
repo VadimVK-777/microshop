3a3df802d1d730d5d38c0467384fc946117a26fc
VADIM <investorprof@gmail.com>
2025-01-31 13:31:28 +0500

    добавил модуль

--- users/crud.py
+++ users/crud.py
@@ -1,12 +1,11 @@
-
-
 from users.schemas import CreateUser
 
 print("Hello World")
 
+
 def create_user(user_in: CreateUser) -> dict:
     user = user_in.model_dump()
     return {
-        'success': True,
-        'user': user,
+        "success": True,
+        "user": user,
     }
--- users/schemas.py
+++ users/schemas.py
@@ -5,6 +5,7 @@ from pydantic import BaseModel, EmailStr, Field
 print("Hello World")
 print("Hello World")
 
+
 class CreateUser(BaseModel):
     # username: str Field(..., min_length=3, max_length=20)
     username: Annotated[str, MinLen(3), MaxLen(20)]
