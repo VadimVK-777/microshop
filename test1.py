4b1a6aa6c9e2e2a6744e3b0182d1d37bf73366b8
VADIM <investorprof@gmail.com>
2025-02-01 12:45:06 +0500

    добавил модуль

---
+++ test1.py
@@ -0,0 +1,44 @@
+from typing import Optional
+
+
+# def say_hi(name: Optional[str]):
+#     if name is not None:
+#         print(f"Hey {name}!")
+#     else:
+#         print("Hello World")
+#
+#
+# say_hi("string")
+# say_hi("")
+# say_hi(None)
+
+
+from typing import Optional
+
+
+def greet(name: Optional[str]) -> str:
+    return f"Hello, {name}!"
+
+
+print(greet(None))  # Ошибка: TypeError
+
+#
+# def get_full_name(a: int, b: int) -> int:
+#     result1 = a + b
+#     return result1
+#
+#
+# def get_full_name1(a, b):
+#     result1 = a + b
+#     return result1
+#
+#
+# print(get_full_name(1, "2"))
+# # print(get_full_name(1, 2))
+#
+# # print(get_full_name1(1, 2))
+# print(get_full_name1([1, 2], 2))
+# # print(get_full_name1([1, 2], "str"))
+# # print(get_full_name1(1, "str"))
+# # print(get_full_name1([1, 2], 2))
+# # print(get_full_name1([1, 2], 2))
--- users/schemas.py
+++ users/schemas.py
@@ -2,9 +2,6 @@ from typing import Annotated
 from annotated_types import MaxLen, MinLen
 from pydantic import BaseModel, EmailStr, Field
 
-print("Hello World")
-print("Hello World")
-
 
 class CreateUser(BaseModel):
     # username: str Field(..., min_length=3, max_length=20)
